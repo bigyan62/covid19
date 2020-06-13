8c5b1f3c031793341605b08edf4857da4b37e422
sitikhusushan <31877565+sitikhusushan@users.noreply.github.com>
2020-06-13 15:33:09 +0545

    footer updated

--- Footer.php
+++ Footer.php
@@ -1,3 +1,62 @@
+    <!-- footer starts form here -->
+    <footer>
+      <div class="subfooter">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3">
+            <img src="image/logo/logo-white.png" alt="coronalogo" />
+            <p>
+              Covidas has been developed on the basis of guidelines from the WHO
+              and WHFW for prevention of all human coronavirus COVID-19
+              diseases.
+            </p>
+            <i class="fab fa-facebook-f"></i>
+            <i class="fab fa-twitter"></i>
+            <i class="fab fa-linkedin-in"></i>
+            <i class="fab fa-youtube"></i>
+          </div>
+          <div class="col-md-3 ">
+            <h3>Situation Updates</h3>
+            <ul>
+              <li> <a href="#">Disease outbreak news</a> </li>
+              <li> <a href="#">COVID-19 situation dashboard</a> </li>
+              <li> <a href="#">Coronavirus pandemic</a> </li>
+              <li><a href="#">Global situation dashboard</a> </li>
+              <li> <a href="#">Youtube live situation reports</a></li>
+            </ul>
+          </div>
+          <div class="col-md-3 ">
+            <h3>Useful Links</h3>
+            <ul>
+              <li> <a href="#">World Health Report</a> </li>
+              <li> <a href="#">Corona Pandemic</a> </li>
+              <li> <a href="#">Latest News</a> </li>
+              <li> <a href="#">Symptoms</a> </li>
+              <li> <a href="#">Prevention</a> </li>
+            </ul>
+          </div>
+          <div class="col-md-3">
+            <h3>COVID-19 Response Fund</h3>
+            <h4>We need your Help</h4>
+            <p>
+              Would You like to Help people? Third World Country need your help
+            </p>
+            <button class="btn btn-primary" type="button"><i class="fas fa-donate"></i> Donate Us</button>
+          </div>
+        </div>
+      </div>
+      </div>
+    </footer>
+    <div class="copyright">
+      <div class="container">
+        <div class="copy">
+        <h4>Copyright &copy; 2020 Marvel Theme. All rights reserved.</h4>
+        <p>All informations are collected from the World Health Organaization (WHO). <br>
+        If you have any concerns please visit the respective website.</p>
+      </div>
+      </div>
+    </div>
+
 <script src="js/jquery-3.1.1.min.js"></script>
 <script src="js/bootstrap.js"></script>
 <script src="js/script.js"></script>
